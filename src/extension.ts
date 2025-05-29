97f0e2869c45357e35bfda299c73b54e46113753
qwqqaqqwq00 <mechulpianus@outlook.com>
2025-05-29 12:42:02 +0800

    feat: rename extension and update display name
    - Change extension name from "music-player" to "siren-music-player"
    - Update display name to "塞壬唱片音乐播放器"
    - Refactor command and view IDs to use new extension name
    - Remove unused helloWorld command and related code

--- src/extension.ts
+++ src/extension.ts
@@ -34,26 +34,12 @@ let playerPanel: vscode.WebviewPanel | undefined;
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "music-player" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('music-player.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from music-player!');
-	});
-
-	context.subscriptions.push(disposable);
 
 	const config = vscode.workspace.getConfiguration('music-player');
 	const savePath = config.get<string>('savePath') || '默认路径';
 
 	// 注册命令：在线播放
-	const playOnline = vscode.commands.registerCommand('music-player.playOnline', async () => {
+	const playOnline = vscode.commands.registerCommand('siren-music-player.playOnline', async () => {
 		const cid = await pickSong();
 		if (!cid) {
 			vscode.window.showWarningMessage('未选择任何歌曲');
